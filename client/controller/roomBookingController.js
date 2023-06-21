96a615d915d8b8036725a9d0a51f03e78cbd09f3
20120585 <73948433+lvthinh100@users.noreply.github.com>
2023-06-21 22:10:28 +0700

    UI for booking ticket feature

--- client/controller/roomBookingController.js
+++ client/controller/roomBookingController.js
@@ -5,10 +5,20 @@ exports.renderRoomBookingPage = (req, res) => {
     userName: "Lê Văn Thịnh",
     checkIn: "25/02/2002",
     checkOut: "27/02/2002",
+    room: 102,
     num: 3,
     status: "checked in",
   };
 
+  /*
+    [{
+      roomId,
+      ticketId,
+      checkIn,
+      checkOut,
+      users: [ {name, cmnd, phone, address} ]
+    }]
+  */
   const tickets = [data, data, data, data, data, data, data];
 
   return res.render("roomBooking", {
